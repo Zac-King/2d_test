2d71abd4e5a8f599a798ae76a26ff760b3d59d79
Zac King <Zacharyking18@yahoo.com>
2016-02-02 17:57:11 -0600

    init push

---
+++ Assets/Player2DController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+[RequireComponent(typeof(Player2D))]
+public class Player2DController : MonoBehaviour
+{
+    private Player2D character;
+    private bool jump;
+
+    private void Awake()
+    {
+        character = GetComponent<Player2D>();
+    }
+
+
+    private void Update()
+    {
+        if (!jump)
+        {
+            // Read the jump input in Update so button presses aren't missed.
+            jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Read the inputs.
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        // Pass all parameters to the character control script.
+        character.Move(h, crouch, jump);
+        jump = false;
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float walkSpeed;
+    public float jumpPower;
+	
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.A)) //Left
+        {
+            //transform.position = new pos;
+        }
+	}
+}
